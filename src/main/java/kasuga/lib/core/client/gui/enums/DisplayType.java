f085b2aa94491991241c38d06b3fc73b42e840fe
TimeBather <open-source@timebather.cn>
2024-04-15 22:43:12 +0800

    feat: implement serializer & deserializer for DisplayType

--- src/main/java/kasuga/lib/core/client/gui/enums/DisplayType.java
+++ src/main/java/kasuga/lib/core/client/gui/enums/DisplayType.java
@@ -2,5 +2,21 @@ package kasuga.lib.core.client.gui.enums;
 
 public enum DisplayType {
     BLOCK,
-    INLINE_BLOCK
+    INLINE_BLOCK;
+
+    @Override
+    public String toString() {
+        return switch (this) {
+            case BLOCK -> "block";
+            case INLINE_BLOCK -> "inline_block";
+        };
+    }
+
+    public static DisplayType fromString(String input) {
+        return switch (input) {
+            case "block" -> BLOCK;
+            case "inline_block" -> INLINE_BLOCK;
+            default -> BLOCK;
+        };
+    }
 }
