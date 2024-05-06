b4a62d99902527e7b9364971cabf7c1368522203
TimeBather <open-source@timebather.cn>
2024-05-06 16:12:20 +0800

    feat: add source to MouseEvent

--- src/main/java/kasuga/lib/core/client/gui/context/MouseEvent.java
+++ src/main/java/kasuga/lib/core/client/gui/context/MouseEvent.java
@@ -3,5 +3,6 @@ package kasuga.lib.core.client.gui.context;
 public record MouseEvent(
         float mouseX,
         float mouseY,
-        int button
+        int button,
+        Object source
 ) {}
