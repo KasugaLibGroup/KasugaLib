961e97c593d1b288a42dd35fb4775b8a7bbc0aed
TimeBather <open-source@timebather.cn>
2024-05-17 00:58:05 +0800

    feat(gui): add GuiManager.create(Source)

--- src/main/java/kasuga/lib/core/client/gui/GuiManager.java
+++ src/main/java/kasuga/lib/core/client/gui/GuiManager.java
@@ -2,6 +2,7 @@ package kasuga.lib.core.client.gui;
 
 import kasuga.lib.core.client.gui.thread.GuiThreadPool;
 import net.minecraft.resources.ResourceLocation;
+import org.graalvm.polyglot.Source;
 
 public class GuiManager {
     protected GuiThreadPool threadPool = new GuiThreadPool();
@@ -12,6 +13,10 @@ public class GuiManager {
 
     public GuiInstance create(ResourceLocation mainFile){
         return new GuiInstance(()->threadPool.createContext(),mainFile);
+    }
+
+    public GuiInstance create(Source mainSource){
+        return new GuiInstance(()->threadPool.createContext(),mainSource);
     }
 
     public void renderTick() {
