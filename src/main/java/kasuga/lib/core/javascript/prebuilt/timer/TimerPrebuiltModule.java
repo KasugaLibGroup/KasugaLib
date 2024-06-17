a90ff63619f73fc1202b6e0bb48db40f4162075d
TimeBather <open-source@timebather.cn>
2024-06-17 22:27:33 +0800

    feat(javascript.timer): add requestTimeout, requestInterval.

--- src/main/java/kasuga/lib/core/javascript/prebuilt/timer/TimerPrebuiltModule.java
+++ src/main/java/kasuga/lib/core/javascript/prebuilt/timer/TimerPrebuiltModule.java
@@ -19,13 +19,24 @@ public class TimerPrebuiltModule implements Tickable, Closeable {
     public void tick() {
         timer.onTick();
     }
+    @HostAccess.Export
+    @HostAccess.DisableMethodScoping
+    public int requestTimeout(KasugaTimer.Callback callback, Value interval){
+        return requestScheduled(KasugaTimer.TimerType.TIMEOUT,callback,interval);
+    }
+
+    @HostAccess.Export
+    @HostAccess.DisableMethodScoping
+    public int requestInterval(KasugaTimer.Callback callback, Value interval){
+        return requestScheduled(KasugaTimer.TimerType.INTERVAL,callback,interval);
+    }
 
     @HostAccess.Export
     @HostAccess.DisableMethodScoping
     public int requestScheduled(KasugaTimer.TimerType type, KasugaTimer.Callback callback, Value interval){
         if(!interval.isNumber())
             return -1;
-        return timer.register(type, callback,Math.max(1,interval.asInt() / 50));
+        return timer.register(type, callback,Math.max(50,interval.asInt() / 50));
     }
 
     @HostAccess.Export
