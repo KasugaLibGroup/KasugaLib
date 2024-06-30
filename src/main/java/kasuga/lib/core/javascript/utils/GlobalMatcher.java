062aa57eae4c478ce30f1720e15cb10041d13651
Goldenfield192 <1437356849@qq.com>
2024-06-30 11:15:52 +0800

    Bug fix

--- src/main/java/kasuga/lib/core/javascript/utils/GlobalMatcher.java
+++ src/main/java/kasuga/lib/core/javascript/utils/GlobalMatcher.java
@@ -19,7 +19,7 @@ public class GlobalMatcher {
     }
 
     public Stream<List<String>> match(Stream<List<String>> input) {
-        return input.map(GlobalMatcher::join).filter(this::validate).map(s ->  List.of(s.split("/")));
+        return input.filter(this::validate);
     }
 
     /**
@@ -28,47 +28,47 @@ public class GlobalMatcher {
      * @param shouldScanChildren A validator for whether the children folders of the folder should be scanned
      * @return All matched files
      */
-    public List<String> match(Function<String, List<String>> input, Predicate<String> shouldScanChildren) {
-        List<String> result = new LinkedList<>();
-        List<String> dir = input.apply("/");
+    public List<List<String>> match(Function<List<String>, List<String>> input, Predicate<List<String>> shouldScanChildren) {
+        List<List<String>> result = new LinkedList<>();
+        List<String> dir = input.apply(List.of("/"));
         if (dir != null) {
             for (String file : dir) {
-                result.addAll(dfs(file, input, shouldScanChildren));
+                List<String> path = new LinkedList<>();
+                path.add(file);
+                result.addAll(dfs(path, input, shouldScanChildren));
             }
         }
         return result;
     }
 
-    private List<String> dfs(String file, Function<String, List<String>> input, Predicate<String> shouldScanChildren) {
-        List<String> result = new LinkedList<>();
+    private List<List<String>> dfs(List<String> file, Function<List<String>, List<String>> input, Predicate<List<String>> shouldScanChildren) {
+        List<List<String>> result = new LinkedList<>();
         List<String> children = input.apply(file);
         if (children != null) {
-            for (String path : children) {
-                if (validate(path)) {
-                    result.add(path);
+            for (String name : children) {
+                file.add(name);
+                if (validate(file)) {
+                    result.add(file);
                 }
-                if (input.apply(path) != null && shouldScanChildren.test(file) && validate(path)) {
-                    result.addAll(dfs(path, input, shouldScanChildren));
+                if (input.apply(file) != null && shouldScanChildren.test(file) && validate(file)) {
+                    result.addAll(dfs(file, input, shouldScanChildren));
                 }
+                file.remove(file.size()-1);
             }
         }
         return result;
     }
 
-    private boolean validate(String path) {
-        if(path.charAt(0) == '/'){//May cause bugs if we don't remove it
-            path = path.substring(1);
-        }
-        String[] parts = path.split("/");
-        if (parts.length == 0) {//Means "/"
+    private boolean validate(List<String> path) {
+        if (path.size() == 0) {//Means "/"
             return false;
         }
         int unmatchCount = 0;//Count unmatched cases
 
         for(List<String> matcher : matchers){
             int i = 0, j = 0;
-            while (i < parts.length && j < matcher.size()) {
-                if (Objects.equals(parts[i], matcher.get(j))) {
+            while (i < path.size() && j < matcher.size()) {
+                if (Objects.equals(path.get(i), matcher.get(j))) {
                     //Matched, pass
                     i++;
                     j++;
@@ -77,11 +77,11 @@ public class GlobalMatcher {
                     i++;
                     j++;
                 } else if (matcher.get(j).equals("**")) {
-                    if (i + 1 < parts.length && j + 1 < matcher.size() && parts[i + 1].equals(matcher.get(j+1))) {
+                    if (i + 1 < path.size() && j + 1 < matcher.size() && path.get(i+1).equals(matcher.get(j+1))) {
                         //The next param matches, "**" ends
                         i++;
                         j++;
-                    } else if (i + 1 >= parts.length || j + 1 >= matcher.size()) {
+                    } else if (i + 1 >= path.size() || j + 1 >= matcher.size()) {
                         //Reached an end
                         return true;
                     } else {
@@ -105,7 +105,9 @@ public class GlobalMatcher {
                 builder.append(str).append("/");
             }
             builder.deleteCharAt(builder.length() - 1);
+            return builder.toString();
+        }else{
+            return "/";
         }
-        return "/";
     }
 }
