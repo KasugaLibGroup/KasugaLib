3839356ae884cf6ae7a9b2a169fba268861e3a1b
MegumiKasuga <1926195023@qq.com>
2024-08-15 19:26:05 +0800

    累积更新

--- src/main/java/kasuga/lib/core/base/UnModeledBlockProperty.java
+++ src/main/java/kasuga/lib/core/base/UnModeledBlockProperty.java
@@ -9,6 +9,13 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Collection;
 import java.util.Optional;
 
+/**
+ * This class is used for get properties that don't link to the block state json file.
+ * For its full codes, see {@link kasuga.lib.mixins.mixin.client.MixinBlockModelShaper}
+ * After some times of modding, I found that this type of property is really necessary.
+ * @param <J> the class of values of the original property.
+ * @param <T> the class of the original property.
+ */
 @Util
 public class UnModeledBlockProperty<J extends Comparable<J>, T extends Property<J>> extends Property<J> {
 
