fbd3a3c83ad3ae3c4d6a1d746f895f7b22ce369c
MegumiKasuga <1926195023@qq.com>
2024-10-29 10:28:26 +0800

    修复：模型不能正确加载Material的问题

---
+++ src/main/java/kasuga/lib/mixins/mixin/client/MixinModelBakery.java
@@ -0,0 +1,20 @@
+package kasuga.lib.mixins.mixin.client;
+
+import kasuga.lib.core.client.model.BedrockModelLoader;
+import net.minecraft.client.resources.model.Material;
+import net.minecraft.client.resources.model.ModelBakery;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Mixin(ModelBakery.class)
+public class MixinModelBakery {
+
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/util/Set;addAll(Ljava/util/Collection;)Z"))
+    private boolean addAll(Set<Material> instance, Collection<Material> es) {
+        return instance.addAll(es) & instance.addAll(BedrockModelLoader.ADDITIONAL_MATERIALS);
+    }
+}
