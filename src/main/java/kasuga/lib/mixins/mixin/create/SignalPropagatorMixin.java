37a9a0ab3871d3186e3c65a2847b1a308ee8c75a
TimeBather <open-source@timebather.cn>
2024-10-29 17:39:33 +0800

    fix: SignalPropagatorMixin refmap -> false

--- src/main/java/kasuga/lib/mixins/mixin/create/SignalPropagatorMixin.java
+++ src/main/java/kasuga/lib/mixins/mixin/create/SignalPropagatorMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = SignalPropagator.class)
+@Mixin(value = SignalPropagator.class,remap = false)
 public class SignalPropagatorMixin {
     @Inject(method = "notifySignalsOfNewNode", at = @At("TAIL"))
     private static void onNotifySignalsOfNewNode(TrackGraph graph, TrackNode node, CallbackInfo callbackInfo){
