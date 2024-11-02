e1601b505a5b8818c44b7557b9ee11761b1567d8
TimeBather <open-source@timebather.cn>
2024-11-02 14:29:57 +0800

    fix: did not notify when signal update

--- src/main/java/kasuga/lib/mixins/mixin/create/SignalPropagatorMixin.java
+++ src/main/java/kasuga/lib/mixins/mixin/create/SignalPropagatorMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = SignalPropagator.class,remap = false)
 public class SignalPropagatorMixin {
-    @Inject(method = "notifySignalsOfNewNode", at = @At("TAIL"))
+    @Inject(method = "notifySignalsOfNewNode", at = @At("RETURN"))
     private static void onNotifySignalsOfNewNode(TrackGraph graph, TrackNode node, CallbackInfo callbackInfo){
         CustomTrackSegmentPropagator.notifyNewNode(graph, node);
     }
