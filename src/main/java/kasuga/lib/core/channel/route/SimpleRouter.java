8fa7f910c47aaa9559448f9966df6317fb03ceb7
TimeBather <open-source@timebather.cn>
2024-11-11 04:49:34 +0800

    fix: add route receiver

--- src/main/java/kasuga/lib/core/channel/route/SimpleRouter.java
+++ src/main/java/kasuga/lib/core/channel/route/SimpleRouter.java
@@ -5,28 +5,37 @@ import kasuga.lib.core.channel.peer.Channel;
 import kasuga.lib.core.channel.peer.ChannelPeer;
 import kasuga.lib.core.channel.peer.ChannelReciever;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Optional;
 
 public class SimpleRouter extends RouteRuleManager implements ChannelReciever {
-    HashMap<Label, ChannelReciever> direct_recievers = new HashMap<>();
+    HashMap<Label, ChannelReciever> directPeers = new HashMap<>();
 
     @Override
     public void $onConnect(Channel channel) {
         Label next = channel.destination().address();
-        if(direct_recievers.containsKey(next)){
-            direct_recievers.get(next).$onConnect(channel);
+        if(directPeers.containsKey(next)){
+            directPeers.get(next).$onConnect(channel);
             return;
         }
+        
+        for (RouteRule rule : rules) {
+            Optional<RouteTarget> target = rule.route(channel);
+            if(target.isPresent()){
+                target.get().distribute(channel, this);
+                return;
+            }
+        }
+        
         channel.close();
     }
 
     public void attach(ChannelPeer peer){
-        direct_recievers.put(peer.getAddress(), peer);
+        directPeers.put(peer.getAddress(), peer);
         peer.setDistributor(this);
     }
 
     public void detach(ChannelPeer peer){
-        direct_recievers.remove(peer.getAddress());
+        directPeers.remove(peer.getAddress());
     }
 }
