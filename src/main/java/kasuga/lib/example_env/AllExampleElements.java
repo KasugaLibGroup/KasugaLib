0d346b225716ad0e1ec8d74edc37618845376814
MegumiKasuga <1926195023@qq.com>
2025-01-22 18:55:52 +0800

    - annotations.

--- src/main/java/kasuga/lib/example_env/AllExampleElements.java
+++ src/main/java/kasuga/lib/example_env/AllExampleElements.java
@@ -175,10 +175,9 @@ public class AllExampleElements {
 //                    .addProperty(BlockBehaviour.Properties::requiresCorrectToolForDrops)
 //                    .defaultBlockItem()
 //                    .tabTo(tab)
-//                    .submit(AllExampleElements.REGISTRY);
+//                    .submit(ExampleMain.testRegistry);
 //
-//    public static final OreReg<DropExperienceBlock> exampleOreConfig =
-//            new OreReg<DropExperienceBlock>("salt_ore")
+//    public static final OreReg<DropExperienceBlock> exampleOreConfig = new OreReg<DropExperienceBlock>("salt_ore")
 //            .setOreBlock(SALT_ORE::getBlock)
 //            .addOreReplaceTarget()
 //            .addDeepSlateReplaceTarget()
@@ -186,7 +185,7 @@ public class AllExampleElements {
 //            .setOreQuantityPerGroup(64)
 //            .setOreDistributionType(PlacedFeatureReg.DistributionType.TRIANGLE)
 //            .setOreAnchorAbsolute(80, -80)
-//            .submit(AllExampleElements.REGISTRY);
+//            .submit(ExampleMain.testRegistry);
 
     public static final MenuReg<GreenAppleMenu, GreenAppleScreen> apple =
             new MenuReg<GreenAppleMenu, GreenAppleScreen>("green_apple_screen")
